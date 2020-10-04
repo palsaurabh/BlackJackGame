2b2c25dbc8a5ccb5931575b056adef47f75eaf96
palsaurabh <palsaurabh16@gmail.com>
2020-10-04 22:27:41 +0530

    Working single player black jack

--- main.cpp
+++ main.cpp
@@ -10,43 +10,75 @@
 
 int main()
 {
-	  Deck deck{};
-//	  deck.print();
-	  deck.shuffle();
-	  deck.print();
+	int someOneBusted{0};
+//	int someOneWon{0};
+	int hitOrstand{-1};
+	Deck deck{};
+	deck.shuffle();
+//	deck.print();
 
-	  Player player;
-	  Player dealer;
+	Player player;
+	Player dealer;
 
-	  player.drawCard(deck);
-	  std::cout<<"Player Score is :"<<player.getScore()<<"\n";
+	dealer.drawCard(deck);
+	std::cout<<"Dealer got: "<<dealer.getScore()<<"\n";
 
-	  dealer.drawCard(deck);
-	  std::cout<<"Dealer Score is :"<<dealer.getScore()<<"\n";
+	player.drawCard(deck);
+	player.drawCard(deck);
+	std::cout<<"You got: "<<player.getScore()<<"\n";
 
-	  player.drawCard(deck);
-	  std::cout<<"Player Score is :"<<player.getScore()<<"\n";
+//	while(!someOneBusted)
+	{
+		while(hitOrstand != 0)
+		{
+			std::cout<<"Hit / stand? Choose 1/0:";
+			std::cin>>hitOrstand;
 
-	  dealer.drawCard(deck);
-	  std::cout<<"Dealer Score is :"<<dealer.getScore()<<"\n";
+			if(hitOrstand == 1)
+			{
+				player.drawCard(deck);
+				std::cout<<"You got: "<<player.getScore()<<"\n";
+				if(player.isBusted())
+				{
+					std::cout<<"You Busted!! You've Lost\n";
+					break;
+				}
+			}
+			else
+			{
+				dealer.drawCard(deck);
+				std::cout<<"Dealer got: "<<dealer.getScore()<<"\n";
 
+				if(dealer.getScore() <= 16)
+				{
+					dealer.drawCard(deck);
+					std::cout<<"Dealer got: "<<dealer.getScore()<<"\n";
 
-	  player.drawCard(deck);
-	  std::cout<<"Player Score is :"<<player.getScore()<<"\n";
+					if(dealer.isBusted())
+					{
+						std::cout<<"Dealer is Busted!! You've won\n";
+						break;
+					}
+				}
 
-	  dealer.drawCard(deck);
-	  std::cout<<"Dealer Score is :"<<dealer.getScore()<<"\n";
+				break;
+			}
+		}
+		someOneBusted = player.isBusted() || dealer.isBusted();
+		if(!someOneBusted)
+		{
+			if(player.getScore() > dealer.getScore())
+			{
+				std::cout<<"You've Won\n";
+			}
+			else
+			{
+				std::cout<<"You've Lost\n";
+			}
+		}
+	}
 
-	  if(player.isBusted())
-	  {
-		  std::cout<<"Player is busted\n";
-	  }
-
-	  if(dealer.isBusted())
-	  {
-		  std::cout<<"Dealer is busted\n";
-	  }
-	  return 0;
+	return 0;
 }
 
 
